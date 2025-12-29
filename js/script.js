5694f47a737779dffc6ecbdcb17edec513ad9c19
TikhonovIvan <ivntik77@gmail.com>
2025-12-29 10:40:25 +0600

    first commit 6

--- js/script.js
+++ js/script.js
@@ -37,3 +37,46 @@ window.addEventListener('scroll', () => {
     profile.classList.remove('hidden');
   }
 });
+
+
+let currentLang = localStorage.getItem('lang') || 'ru';
+
+Promise.all([
+  fetch('/locales/ru.json').then(res => res.json()),
+  fetch('/locales/en.json').then(res => res.json())
+]).then(([ru, en]) => {
+
+  i18next.init({
+    lng: currentLang,
+    fallbackLng: 'ru',
+    resources: {
+      ru: { translation: ru },
+      en: { translation: en }
+    }
+  }, updateContent);
+
+});
+
+// обновление текста
+function updateContent() {
+  document.querySelectorAll('[data-i18n]').forEach(el => {
+    el.textContent = i18next.t(el.dataset.i18n);
+  });
+}
+
+// кнопка языка (одна строка)
+document.getElementById('langToggle').addEventListener('click', () => {
+  currentLang = currentLang === 'ru' ? 'en' : 'ru';
+  i18next.changeLanguage(currentLang, updateContent);
+  updateLangUI();
+  localStorage.setItem('lang', currentLang);
+});
+
+// UI кнопки
+function updateLangUI() {
+  document.querySelector('.ru').classList.toggle('none', currentLang !== 'ru');
+  document.querySelector('.en').classList.toggle('none', currentLang !== 'en');
+}
+
+updateLangUI();
+
